b9b1864decbc762432486db65c84cbf7bf961c6c
yang-hsiang chang <leo424y@gmail.com>
2019-01-30 10:12:57 +0800

    show pub_key

--- src/views/login/screen4.js
+++ src/views/login/screen4.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   View,
   Text,
+  TextInput,
   ImageBackground,
   Dimensions,
 } from 'react-native';
@@ -17,15 +18,34 @@ const SCREEN_HEIGHT = Dimensions.get('window').height;
 var keypair = require('keypair');
 
 export default class LoginScreen2 extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+        key: '',
+      }
+    }
+  getKey() 
+  {  
+    var key = keypair({bits: 256})
+    this.setState({
+      key: key,
+    });
+    console.log(this.state.key.public);
+    console.log(this.state.key.private);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground style={styles.bgImage}>
         {/* source={BG_IMAGE}  */}
           <Text style={styles.loginText}>啟動碼</Text>
-          <Text style={styles.travelText}>34393adfjJdi3jk9f</Text>
+          <Text style={styles.travelText}>{this.state.key.public}</Text>
+          {/* <TextInput style={styles.travelText}>{this.state.key.public}</TextInput> */}
           <Button
-                onPress={() => {console.log(keypair())}}
+                // onPress={() => {var key = keypair({bits: 256}); console.log(key)}}
+                onPress={() => {this.getKey()}}
                 title="通知啟動"
                 activeOpacity={1}
                 underlayColor="transparent"
